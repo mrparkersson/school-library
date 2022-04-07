93ab7e7f8ca7d8627e023a013503a95faadb777e
mrparkersson <amoparkerprince78@gmail.com>
2022-04-07 15:50:20 +0000

    Add getters and setters for rental

--- rental.rb
+++ rental.rb
@@ -1,7 +1,17 @@
-class Rental
-  def initialize(date)
-    @date = date
-  end
+require_relative './book'
+require_relative './person'
 
+class Rental
+  attr_reader :book, :person
   attr_accessor :date
+
+  def initialize(person, book, date)
+    @date = date
+
+    @person = person
+    person.rentals.push(self)
+
+    @book = book
+    book.rentals.push(self)
+  end
 end
